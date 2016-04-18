2c79dff8cad22bc37d767d651b98fc2f0d320a4a
Kang-Hye-Na <dtm.03dhcntt2.0350080074@gmail.com>
2016-04-18 12:30:08 +0700

    First Commit

---
+++ vdfor.java
@@ -0,0 +1,13 @@
+package BaiTap1;
+
+public class vdfor {
+
+	public static void main(String[] args) {
+		int step =1;
+		for(int value =0; value<10 ; value = value + 3){
+		System.out.println("step ="+ step+ " value ="+ value );
+        step=step+1;
+		}
+	}
+
+}
